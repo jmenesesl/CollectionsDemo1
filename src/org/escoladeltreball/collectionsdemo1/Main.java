15705ea26fc92b8e0226a9a8dff86cec32dd15d0
iaw26068632 <iaw26068632@h18.informatica.escoladeltreball.org>
2017-10-04 17:48:27 +0200

    implemented subLlista

--- src/org/escoladeltreball/collectionsdemo1/Main.java
+++ src/org/escoladeltreball/collectionsdemo1/Main.java
@@ -14,10 +14,31 @@ import java.util.List;
  */
 public class Main {
 
+	public static List<String> subLlista(List<String> list, String expressio) {
+		List<String> result = new LinkedList();
+		
+		/*
+		 * for (String string: list) {
+		 * 	if (string.indexOf(expressio) != -1) {   // SI SE ENCUENTRA LA EXPRESION EN EL STRING
+		 * 	result.add(string);
+		 * 	}
+		 * }
+		 * 
+		 * Otra: string.startsWith(expressio)
+		 */
+		for (String string: list) {			
+			if (string.substring(0, expressio.length()).equals(expressio)) {
+				result.add(string);
+			}
+		}
+		return result;
+	}
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		
+		
 //		List<String> list = new ArrayList();
 		List<String> list = new LinkedList<>();
 		
@@ -27,16 +48,27 @@ public class Main {
 		list.add("Echo");
 		list.add("Delta");
 		list.add("Zulus");
+		list.add("Albacete");
+		list.add("Alava");
 		
 		list.remove("Charlie");
 		
+		// Ordenar una lista		
 		Collections.sort(list);
+		
+		
 		// Listar una lista
 		
 		for(String string: list) {
 			System.out.println(string);
 		}
 		
+		List<String> result = subLlista(list, "Al");
+		
+		System.out.println("\n\nSUBLLISTA\n******************\n");
+		for(String string: result) {
+			System.out.println(string);
+		}
 	}
 
 }
