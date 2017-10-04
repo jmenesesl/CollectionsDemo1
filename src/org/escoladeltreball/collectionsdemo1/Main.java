3224358f3a3063f591c6ba5785f9ea98402a4d7f
iaw26068632 <iaw26068632@h18.informatica.escoladeltreball.org>
2017-10-04 17:15:57 +0200

    linkedlist Introduced

--- src/org/escoladeltreball/collectionsdemo1/Main.java
+++ src/org/escoladeltreball/collectionsdemo1/Main.java
@@ -5,6 +5,7 @@ package org.escoladeltreball.collectionsdemo1;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -17,7 +18,8 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		List<String> list = new ArrayList();
+//		List<String> list = new ArrayList();
+		List<String> list = new LinkedList<>();
 		
 		list.add("Alpha");
 		list.add("Charlie");
